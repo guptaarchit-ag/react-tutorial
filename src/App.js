1d0a7616327168f32a137616c6f2502b711d9f6a
Archit Gupta <pi@raspberrypi>
2020-06-28 17:02:04 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React, {Component} from 'react';
+import Table from './Table';
+import Form from './Form';
+
+class App extends Component{
+    // This object will contain properties for everything you want to store in the state.
+    // In this case it is characters
+    state = {
+        characters: [],
+    };
+    removeCharacter = index => {
+        const {characters} = this.state;
+        this.setState({
+            characters: characters.filter((character, i) => {
+                return i !== index;
+            })
+        });
+    }
+    handleSubmit = character => {
+        this.setState({characters: [...this.state.characters, character]});
+    }
+
+    render(){
+        const {characters} = this.state;
+        return(
+            <div className = "container">
+                <Table characterData = {characters} removeCharacter = {this.removeCharacter} />
+                <Form handleSubmit = {this.handleSubmit}/>
+                
+            </div>
+        );
+    }
+    
+}
+
+//       return(
+//         <div className = "container">
+//           <Table characterData = {characters} />
+//         </div>
+//       )
+//     }
+// }
+
+export default App;
